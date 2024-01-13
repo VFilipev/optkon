ce0d3d4d80adeb6fec25abefc251a72f12bbdee1
VFilipev <vasiliy20122009@mail.ru>
2024-01-13 16:14:05 +0500

    fix

---
+++ catalog/static/View-2f8d47a6.js
@@ -0,0 +1,1 @@
+import{r as o,o as r,c as t}from"./index-5e175bee.js";const a={__name:"View",setup(c){return(n,_)=>{const e=o("router-view");return r(),t(e)}}};export{a as default};
--- catalog/static/View-904c7cd6.js
+++
@@ -1,1 +0,0 @@
-import{r as o,o as r,c as t}from"./index-0a93e18d.js";const a={__name:"View",setup(c){return(n,_)=>{const e=o("router-view");return r(),t(e)}}};export{a as default};
