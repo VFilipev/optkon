b571f0201d7d4d8643484e9872196ebf43613133
VFilipev <vasiliy20122009@mail.ru>
2024-01-16 20:34:10 +0500

    fix

--- order/models.py
+++ order/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from catalog.models import Product
 # from django.contrib.sessions.models import Session
-from django.template.loader import render_to_string
-from django.conf import settings
-from django.core.mail import send_mail
 
 # Create your models here.
 
@@ -20,11 +17,9 @@ class Order(models.Model):
     ('D','Отменен'),
     )
     status = models.CharField(choices=STATUS_CHOICE,max_length=2,default="N")
-    def send_mail(self):
-        msg = render_to_string('order_mail.html', {'order':self})
-        send_mail('[Заказ] %d' %self.id , msg, settings.EMAIL_HOST_USER, [settings.ADMIN_EMAIL], html_message=msg)
+    
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,on_delete=models.PROTECT)
-    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,blank=True,null=True,on_delete=models.CASCADE)
     count = models.IntegerField()
